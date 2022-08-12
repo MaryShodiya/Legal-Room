82a8219fe671c51b89c803ef533f37b5bf5ca013
MaryShodiya <marysho57@gmail.com>
2022-08-12 14:46:30 +0100

    solved

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express') //connect express to server
 const app = express() //connect application to express server
 const MongoClient = require('mongodb').MongoClient //connect MongoDB to server
-const PORT = 8000 //port localhost
+const PORT = 2124 //port localhost
 require('dotenv').config() //database string
 
 let db,
@@ -61,13 +61,13 @@ app.post('/addQuestion', (req, res) =>{
 
 
 app.put('/addOneLike', (req, res) =>{
-    db.collection('questions').updateOne({questionHeader : req.body.questionHeaderS, questionBody: req.body.questionBodyS,likes: req.body.likesS},{
+    db.collection('questions').updateOne({questionHeader : req.body.questionHeaderS, questionBody: req.body.questionBodyS},{
          $set: {
              likes:req.body.likesS + 1
          }
      }, {
          sort: {_id: -1},
-         upsert: true 
+         upsert: true
      })
      .then(result => {
          console.log('Added One Like')
@@ -106,66 +106,7 @@ app.delete('/deleteQuestionAdded', (req, res) =>{
     .catch(err => console.error(err))
 })
 
-/*let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'security' //name of database
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName) 
-    })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-
-app.get('/',(request, response)=>{
-    db.collection('keys').find().sort({likes: -1}).toArray()
-    .then(data => {
-        response.render('index.ejs', { info: data })
-    })
-    .catch(error => console.error(error))
-})
-
-app.post('/addKey', (request, response) => {
-    db.collection('keys').insertOne({appName: request.body.appName,
-    securityCode: request.body.securityCode, likes: 0})
-    .then(result => {
-        console.log('Rapper Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
-
-app.put('/addOneLike', (request, response) => {
-    db.collection('keys').updateOne({appName: request.body.appNameS, securityCode: request.body.securityCodeS,likes: request.body.likesS},{
-        $set: {
-            likes:request.body.likesS + 1
-          }
-    },{
-        sort: {_id: -1},
-        upsert: true
-    })
-    .then(result => {
-        console.log('Added One Like')
-        response.json('Like Added')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.delete('/deleteKey', (request, response) => {
-    db.collection('keys').deleteOne({appName: request.body.appNameS})
-    .then(result => {
-        console.log('Rapper Deleted')
-        response.json('Rapper Deleted')
-    })
-    .catch(error => console.error(error))
-
-})*/
 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
