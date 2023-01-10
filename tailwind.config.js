58ba08ca1ed0018ecca8c14e3f83793b10098cbb
MaryShodiya <marysho57@gmail.com>
2023-01-10 16:30:22 +0100

    changed signup styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./views/**/*.ejs"],
   
   theme: {
-    extend: {},
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        purple: colors.purple,
+        rebeccapurple: colors.rebeccapurple,
+        blueviolet: colors.blueviolet,
+        darkorchid: colors.darkorchid,
+        mediumpurple: colors.mediumpurple,
+        whitesmoke: colors.whitesmoke,
+      },
   },
   plugins: [
     require("daisyui"),
